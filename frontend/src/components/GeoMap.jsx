767c28bdf897f2bda2278b3a6f1b0993325351a6
ShikharSinghNegi <shikhar.negi@ucdconnect.ie>
2025-07-27 20:49:37 +0100

    add changes to dockerize the program

--- frontend/src/components/GeoMap.jsx
+++ frontend/src/components/GeoMap.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export default function GeoMap({ siteData }) {
   if (!siteData || siteData.length === 0) return null;
